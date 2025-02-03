6fdcb4308c365ec32f4ee9e801298593fc71042a
Shuceyb-Abdi <shuceybabdi361@gmail.com>
2025-02-03 23:23:49 +0300

    commit

--- lib/Widgets/PopularItemsWidget.dart
+++ lib/Widgets/PopularItemsWidget.dart
@@ -26,6 +26,54 @@ class PopularItemsWidget extends StatelessWidget{
                     )
                   ] 
                 ),
+
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    //crossAxisAligment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        //alignment: Alignment.center,
+                        child: Image.asset(
+                          "images/burger.png",
+                          height: 130,
+                        ),
+                      ),
+                      Text(
+                        "Hot Burger", 
+                        style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+
+                      ),
+                      ),
+                      SizedBox(height: 4),
+                       Text(
+                        "Teste Our Hot Burger", 
+                        style: TextStyle(
+                        fontSize: 15,
+                        //fontWeight: FontWeight.bold,
+
+                      ),
+                      ),
+                      SizedBox(height: 12),
+                      Row(
+                        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "\$10",
+                            style: TextStyle(
+                               color: Colors.red,
+                            fontWeight: FontWeight.bold
+                             ),
+                           
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                
+                ),
               ),
             ),
           ],
